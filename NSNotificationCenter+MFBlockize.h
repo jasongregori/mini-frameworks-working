3c3df01aad1233b80cadeba51d846a9c429c32be
Jason Gregori <jasongregori@gmail.com>
2011-07-19 16:18:33 -0700

    sweet notification center category for using blocks and not having to clean up after :)

---
+++ NSNotificationCenter+MFBlockize.h
@@ -0,0 +1,27 @@
+//
+//  NSNotificationCenter+MFBlockize.h
+//  zabbi
+//
+//  Created by Jason Gregori on 7/18/11.
+//  Copyright 2011 Jason Gregori. All rights reserved.
+//
+
+// @MF: Observe notifications without having to worry about cleaning up after.
+
+/*
+ 
+ When you add an observance using one of these methods, it is linked to the observer.
+ You do not have to worry about removing the observance later.
+ The observance will automatically be removed when the observer is dealloced.
+ Adding an observance with these methods will not retain the observer and is passed to your block so you do not have to create a weak version.
+ NB: you may only observe a specific notification name once, adding another replaces the first.
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSNotificationCenter (MFBlockize)
++ (void)mfAddObserver:(id)observer name:(NSString *)name object:(id)obj queue:(NSOperationQueue *)queue usingBlock:(void (^)(id weakObserver, NSNotification *n))block;
+// uses nil for object and queue
++ (void)mfAddObserver:(id)observer name:(NSString *)name usingBlock:(void (^)(id weakObserver, NSNotification *n))block;
+@end
---
+++ NSNotificationCenter+MFBlockize.m
@@ -0,0 +1,46 @@
+//
+//  NSNotificationCenter+MFBlockize.m
+//  zabbi
+//
+//  Created by Jason Gregori on 7/18/11.
+//  Copyright 2011 Jason Gregori. All rights reserved.
+//
+
+#import "NSNotificationCenter+MFBlockize.h"
+
+#import <objc/runtime.h>
+
+@interface __NSNotificationCenter_MFBlockize_Helper : NSObject
+@property (nonatomic, retain) id realObserver;
+@end
+
+@implementation NSNotificationCenter (MFBlockize)
+
++ (void)mfAddObserver:(id)observer name:(NSString *)name object:(id)obj queue:(NSOperationQueue *)queue usingBlock:(void (^)(id observer, NSNotification *n))block {
+    __NSNotificationCenter_MFBlockize_Helper *helper = [[[__NSNotificationCenter_MFBlockize_Helper alloc] init] autorelease];
+    __block id weakObserver = observer;
+    helper.realObserver = [[NSNotificationCenter defaultCenter] addObserverForName:name object:obj queue:queue usingBlock:^(NSNotification *n) {
+        block(weakObserver, n);
+    }];
+    objc_setAssociatedObject(observer, name, helper, OBJC_ASSOCIATION_RETAIN);
+}
+
++ (void)mfAddObserver:(id)observer name:(NSString *)name usingBlock:(void (^)(id observer, NSNotification *n))block {
+    [self mfAddObserver:observer name:name object:nil queue:nil usingBlock:block];
+}
+
+@end
+
+@implementation __NSNotificationCenter_MFBlockize_Helper
+@synthesize realObserver;
+
+- (void)dealloc {
+    // remove real observer!
+    if (self.realObserver) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.realObserver];
+        self.realObserver = nil;
+    }
+    [super dealloc];
+}
+
+@end
