10e08d3b8f1454f79da4cabe57a377a6fe836c60
Jason Gregori <jasongregori@gmail.com>
2011-07-18 17:33:32 -0700

    MFEasyInit mini framework

---
+++ MFEasyInit.h
@@ -0,0 +1,38 @@
+//
+//  MFEasyInit.h
+//  zabbi
+//
+//  Created by Jason Gregori on 7/18/11.
+//  Copyright 2011 Jason Gregori. All rights reserved.
+//
+
+// @mf: A collection of categories with one call methods for alloc init autoreleasing things.
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSObject (MFEasyInit)
+// alloc init autoreleases another instance of your object
++ (id)mfAnother;
+@end
+
+@interface UIActivityIndicatorView (MFEasyInit)
++ (id)mfAnotherWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style;
++ (id)mfAnotherWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style animating:(BOOL)animating;
+@end
+
+@interface UIAlertView (MFEasyInit)
+// warning: the otherButtonTitles part has not been tested yet
++ (id)mfAnotherWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+@end
+
+@interface UIBarButtonItem (MFEasyInit)
++ (id)mfAnotherWithSystemItem:(UIBarButtonSystemItem)systemItem target:(id)target action:(SEL)action;
++ (id)mfAnotherWithCustomView:(UIView *)customView;
++ (id)mfAnotherWithImage:(UIImage *)image style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action;
++ (id)mfAnotherWithTitle:(NSString *)title style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action;
+@end
+
+@interface UINavigationController (MFEasyInit)
++ (id)mfAnotherWithRootViewController:(UIViewController *)rootViewController;
+@end
---
+++ MFEasyInit.m
@@ -0,0 +1,65 @@
+//
+//  MFEasyInit.m
+//  zabbi
+//
+//  Created by Jason Gregori on 7/18/11.
+//  Copyright 2011 Jason Gregori. All rights reserved.
+//
+
+#import "MFEasyInit.h"
+
+@implementation NSObject (MFEasyInit)
++ (id)mfAnother {
+    return [[[self alloc] init] autorelease];
+}
+@end
+
+@implementation UIActivityIndicatorView (MFEasyInit)
++ (id)mfAnotherWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style {
+    return [[[self alloc] initWithActivityIndicatorStyle:style] autorelease];
+}
++ (id)mfAnotherWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style animating:(BOOL)animating {
+    UIActivityIndicatorView *aiv = [self mfAnotherWithActivityIndicatorStyle:style];
+    if (animating) {
+        [aiv startAnimating];
+    }
+    return aiv;
+}
+@end
+
+@implementation UIAlertView (MFEasyInit)
++ (id)mfAnotherWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
+    UIAlertView *alert = [[[self alloc] initWithTitle:title message:message delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:nil] autorelease];
+    // ## we must add the other button titles here since we cannot pass the varargs into the init method
+    va_list buttons;
+    va_start(buttons, otherButtonTitles);
+    NSString *name = otherButtonTitles;
+    while (name) {
+        [alert addButtonWithTitle:name];
+        name = va_arg(buttons, NSString *);
+    }
+    va_end(buttons);
+    return alert;
+}
+@end
+
+@implementation UIBarButtonItem (MFEasyInit)
++ (id)mfAnotherWithSystemItem:(UIBarButtonSystemItem)systemItem target:(id)target action:(SEL)action {
+    return [[[self alloc] initWithBarButtonSystemItem:systemItem target:target action:action] autorelease];
+}
++ (id)mfAnotherWithCustomView:(UIView *)customView {
+    return [[[self alloc] initWithCustomView:customView] autorelease];
+}
++ (id)mfAnotherWithImage:(UIImage *)image style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action {
+    return [[[self alloc] initWithImage:image style:style target:target action:action] autorelease];
+}
++ (id)mfAnotherWithTitle:(NSString *)title style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action {
+    return [[[self alloc] initWithTitle:title style:style target:target action:action] autorelease];
+}
+@end
+
+@implementation UINavigationController (MFEasyInit)
++ (id)mfAnotherWithRootViewController:(UIViewController *)rootViewController {
+    return [[[self alloc] initWithRootViewController:rootViewController] autorelease];
+}
+@end
