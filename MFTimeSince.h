c04d389e7d36c13dd5242547c1bde752da1abcb1
Jason Gregori <jasongregori@gmail.com>
2011-09-16 12:25:34 -0700

    MFTimeSince: added example

--- MFTimeSince.h
+++ MFTimeSince.h
@@ -14,6 +14,13 @@
  All localization are passed the number of units as an int (%i).
  If no localization is found that fits a time, the highest localization will be used.
  
+ Example:
+
+   [MFTimeSince setLocalizationString:NSLocalizedString(@"%is", @"%i seconds") forUpToThisManyUnits:60 secondsInUnit:1];
+   [MFTimeSince setLocalizationString:NSLocalizedString(@"%im", @"%i minutes") forUpToThisManyUnits:60 secondsInUnit:60];
+   [MFTimeSince setLocalizationString:NSLocalizedString(@"%ih", @"%i hours") forUpToThisManyUnits:24 secondsInUnit:60*60];
+   [MFTimeSince setLocalizationString:NSLocalizedString(@"%id", @"%i days") forUpToThisManyUnits:DBL_MAX secondsInUnit:24*60*60];
+
  */
 
 #import <Foundation/Foundation.h>
