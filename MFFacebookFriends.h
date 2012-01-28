7cc524e5e361d1f123518f91a12db74a4f5c6188
Jason Gregori <jasongregori@gmail.com>
2012-01-27 16:43:50 -0800

    working on MFFacebookFriends

--- MFFacebookFriends.h
+++ MFFacebookFriends.h
@@ -9,29 +9,36 @@
 // @mf: An api for caching facebook friends
 
 #import <Foundation/Foundation.h>
+#import "Facebook.h"
 
 /*
  
- Friendlies are always returned alphabetized according to the user's contact settings
+ - Use the category method to get the mfFriends instance.
+ - Friends are always returned alphabetized according to the user's contact settings.
+ - By default, information gathered for each friend are is_app_user, first_name, last_name, uid, pic_square, and name.
+   You can change it by setting `informationToGather`. See https://developers.facebook.com/docs/reference/fql/user/ for keys.
  
  */
 
-#error How am i going to know which Facebook to use?
-// maybe just setFacebook:?
-// another option is a category but that seems like a lot of work
+@class MFFacebookFriends;
+
+@interface Facebook (MFFacebookFriends)
+@property (nonatomic, readonly) MFFacebookFriends *mfFriends;
+@end
 
 @interface MFFacebookFriends : NSObject
+@property (nonatomic, copy) NSArray *informationToGather;
 
-+ (NSDate *)lastRefreshedDate;
+- (NSDate *)lastRefreshedDate;
 
 // this method will give you the cached friends. if they aren't loaded, this will load them.
 // retain the object returned, if you release the object, your block will not be called.
-+ (id)getFriends:(void (^)(NSArray *friends, NSString *error))block;
+- (id)getFriends:(void (^)(NSArray *friends, NSString *error))block;
 // this method will reload friends even if they are cached.
 // retain the object returned, if you release the object, your block will not be called.
-+ (id)refreshFriends:(void (^)(NSArray *friends, NSString *error))block;
+- (id)refreshFriends:(void (^)(NSArray *friends, NSString *error))block;
 
 // this will clear the cached friends. current requests will still finish but their results will not be cached.
-+ (void)clearCachedFriends;
+- (void)clearCachedFriends;
 
 @end
--- MFFacebookFriends.m
+++ MFFacebookFriends.m
@@ -11,26 +11,49 @@
 #import "Facebook.h"
 
 #import <AddressBook/AddressBook.h>
+#import <objc/runtime.h>
 
 @interface __MFFacebookFriends_OnDealloc : NSObject
 @property (copy) void (^performOnDeallocBlock)();
 + (id)performOnDealloc:(void (^)())block;
 @end
 
+@interface MFFacebookFriends () <FBRequestDelegate> {
+    BOOL __cacheCleared;
+    NSMutableArray *__friends;
+    BOOL __friendsAreLoaded;
+    NSCountedSet *__friendsLoadBlocks;
+    FBRequest *__friendsRequest;
+    NSDate *__lastRefreshedDate;    
+}
+@property (nonatomic, unsafe_unretained) Facebook *facebook;
+@end
+
+@implementation Facebook (MFFacebookFriends)
+
+- (MFFacebookFriends *)mfFriends {
+    MFFacebookFriends *f;
+    static char associationKey;
+    f = objc_getAssociatedObject(self, &associationKey);
+    if (!f) {
+        f = [MFFacebookFriends new];
+        f.facebook = self;
+        objc_setAssociatedObject(self, &associationKey, f, OBJC_ASSOCIATION_RETAIN);
+    }
+    return f;
+}
+
+@end
+
 @implementation MFFacebookFriends
+@synthesize facebook;
+@synthesize informationToGather;
 
-static BOOL __cacheCleared;
-static NSMutableArray *__friends;
-static BOOL __friendsAreLoaded;
-static NSCountedSet *__friendsLoadBlocks;
-static FBRequest *__friendsRequest;
-static NSDate *__lastRefreshedDate;
-
-+ (NSDate *)lastRefreshedDate {
+- (NSDate *)lastRefreshedDate {
     return __lastRefreshedDate;
 }
 
-+ (id)getFriends:(void (^)(NSArray *friends, NSString *error))block {
+- (id)getFriends:(void (^)(NSArray *friends, NSString *error))block {
     if (__friendsAreLoaded) {
         if (block) {
             block(__friends, nil);
@@ -40,7 +63,7 @@ static NSDate *__lastRefreshedDate;
     return [self refreshFriends:block];
 }
 
-+ (id)refreshFriends:(void (^)(NSArray *friends, NSString *error))block {
+- (id)refreshFriends:(void (^)(NSArray *friends, NSString *error))block {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         __friendsLoadBlocks = [NSCountedSet set];
@@ -54,7 +77,14 @@ static NSDate *__lastRefreshedDate;
     }
     
     if (!__friendsRequest) {
-        __friendsRequest = [Facebook 
+        NSString *fql = [NSString stringWithFormat:@"SELECT %@ FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())",
+                         ([self.informationToGather count]
+                          ? [self.informationToGather componentsJoinedByString:@", "]
+                          : @"is_app_user, first_name, last_name, uid, name, pic_square")];
+        __friendsRequest = [self.facebook requestWithGraphPath:@"fql"
+                                                     andParams:[NSDictionary dictionaryWithObject:fql
+                                                                                           forKey:@"q"]
+                                                   andDelegate:self];
         
         
         __friendsRequest = [[ZAZabbiAPI api] requestWithPath:@"friends"
@@ -103,11 +133,55 @@ static NSDate *__lastRefreshedDate;
 }
 
 // this will clear the cached friends. current requests will still finish but their results will not be cached.
-+ (void)clearCachedFriends {
+- (void)clearCachedFriends {
     __cacheCleared = YES;
     __friends = nil;
     __friendsAreLoaded = NO;
     __lastRefreshedDate = nil;
+}
+
+#pragma mark - FBRequestDelegate
+
+- (void)request:(FBRequest *)request didLoad:(id)result {
+    // make sure we get an array back
+    if (![result isKindOfClass:[NSArray class]]
+        || [result count] == 0) {
+        result = nil;
+    }
+    // sort the friends
+    BOOL sortByFirstName = ABPersonGetSortOrdering() == kABPersonSortByFirstName;
+    NSArray *friends = [result sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        if (sortByFirstName) {
+            return [[obj1 zaExpressionCreatorSortStringForFirstName]
+                    localizedCompare:
+                    [obj2 zaExpressionCreatorSortStringForFirstName]];
+        }
+        return [[obj1 zaExpressionCreatorSortStringForLastName]
+                localizedCompare:
+                [obj2 zaExpressionCreatorSortStringForLastName]];
+    }];
+    
+    if (!__cacheCleared) {
+        // if the cache is cleared dont cache these
+        __friends = [friends mutableCopy];
+        __friendsAreLoaded = YES;
+        __lastRefreshedDate = [NSDate date];
+    }
+    for (void (^loadBlock)(NSArray *friends, NSString *error) in __friendsLoadBlocks) {
+        loadBlock(friends, nil);
+    }
+    [__friendsLoadBlocks removeAllObjects];
+    
+    __friendsRequest = nil;
+}
+
+- (void)request:(FBRequest *)request didFailWithError:(NSError *)error {
+    for (void (^loadBlock)(NSArray *friends, NSString *error) in __friendsLoadBlocks) {
+        loadBlock(nil, error);
+    }
+    [__friendsLoadBlocks removeAllObjects];
+    
+    __friendsRequest = nil;
 }
 
 @end
