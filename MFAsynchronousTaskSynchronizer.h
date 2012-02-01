f617061d31e783503790236d6dab2272306dd141
Jason Gregori <jasongregori@gmail.com>
2012-02-01 14:51:27 -0800

    MFAsynchronousTaskSynchronizer tentatively complete
    not yet tested

--- MFAsynchronousTaskSynchronizer.h
+++ MFAsynchronousTaskSynchronizer.h
@@ -12,18 +12,18 @@
  
  - A task takes a `finished` block. Once the task is done, a task must call the `finished` block.
  - A task may return an object if it wants. That object is retained until the task is cancelled or finished.
+ - Your tasks and cancel blocks will be run in the order they are added.
 
  - If the synchronizer is cancelled, all tasks that are not done are cancelled.
  - If a task is cancelled, you must not call the `finished` block.
+ - If a task is finished, your cancel block will not be called.
  
  - You may not add any more tasks after start tasks is called.
- - The synchronizer is automatically cancelled if it deallocs.
+ - The synchronizer is automatically cancelled if it is deallocated.
  
  */
 
 #import <Foundation/Foundation.h>
-
-typedef void (^MFAsynchronousTaskSynchronizerFinishedBlock)();
 
 @interface MFAsynchronousTaskSynchronizer : NSObject
 
