b2b6a85eb121e36c437947145da0109e901a69cb
Jason Gregori <jasongregori@gmail.com>
2012-04-17 18:12:52 -0700

    TTTAttributedLabel: replaced didSelectLinkWithTextCheckingResult:

---
+++ TTTAttributedLabel+MFBlockize.h
@@ -0,0 +1,13 @@
+//
+//  TTTAttributedLabel+MFBlockize.h
+//  zabbi
+//
+//  Created by Jason Gregori on 4/17/12.
+//  Copyright (c) 2012 Jason Gregori. All rights reserved.
+//
+
+#import "TTTAttributedLabel.h"
+
+@interface TTTAttributedLabel (MFBlockize)
+
+@end
---
+++ TTTAttributedLabel+MFBlockize.m
@@ -0,0 +1,13 @@
+//
+//  TTTAttributedLabel+MFBlockize.m
+//  zabbi
+//
+//  Created by Jason Gregori on 4/17/12.
+//  Copyright (c) 2012 Jason Gregori. All rights reserved.
+//
+
+#import "TTTAttributedLabel+MFBlockize.h"
+
+@implementation TTTAttributedLabel (MFBlockize)
+
+@end
