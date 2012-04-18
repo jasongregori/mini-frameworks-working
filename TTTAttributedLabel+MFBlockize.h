c17f94406767dedf36baf34025bfd479df459c5c
Jason Gregori <jasongregori@gmail.com>
2012-04-17 18:13:38 -0700

    TTTAttributedLabel+MFBlockize

--- TTTAttributedLabel+MFBlockize.h
+++ TTTAttributedLabel+MFBlockize.h
@@ -9,5 +9,7 @@
 #import "TTTAttributedLabel.h"
 
 @interface TTTAttributedLabel (MFBlockize)
-
+// setting this block sets the label's delegate. if you change the delegate after, your block will not be called.
+// setting this to nil clears out the block and delegate.
+- (void)mfSetDidSelectLinkWithTextCheckingResultBlock:(void (^)(NSTextCheckingResult *result))block;
 @end
--- TTTAttributedLabel+MFBlockize.m
+++ TTTAttributedLabel+MFBlockize.m
@@ -8,6 +8,40 @@
 
 #import "TTTAttributedLabel+MFBlockize.h"
 
+#import <objc/runtime.h>
+
+@interface __TTTAttributedLabel_MFBlockize_Helper : NSObject <TTTAttributedLabelDelegate>
+@property (nonatomic, copy) void (^didSelectLinkWithTextCheckingResultBlock)(NSTextCheckingResult *result);
+@end
+
 @implementation TTTAttributedLabel (MFBlockize)
+
+- (void)mfSetDidSelectLinkWithTextCheckingResultBlock:(void (^)(NSTextCheckingResult *result))block {
+    
+    __TTTAttributedLabel_MFBlockize_Helper *helper;
+    
+    if (block) {
+        // create helper
+        helper = [__TTTAttributedLabel_MFBlockize_Helper new];
+        helper.didSelectLinkWithTextCheckingResultBlock = block;
+    }
+    
+    self.delegate = helper;
+    
+    // retain the helper
+    static char helperKey;
+    objc_setAssociatedObject(self, &helperKey, helper, OBJC_ASSOCIATION_RETAIN);
+}
+
+@end
+
+@implementation __TTTAttributedLabel_MFBlockize_Helper
+@synthesize didSelectLinkWithTextCheckingResultBlock;
+
+- (void)attributedLabel:(TTTAttributedLabel *)label didSelectLinkWithTextCheckingResult:(NSTextCheckingResult *)result {
+    if (self.didSelectLinkWithTextCheckingResultBlock) {
+        self.didSelectLinkWithTextCheckingResultBlock(result);
+    }
+}
 
 @end
